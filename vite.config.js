a55d7de94db812ebe2c9344de3732e8d04bdc9b7
Andy Anderson <relic606@yahoo.com>
2023-09-27 16:13:42 -0600

    troubleshooting

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: "/projects/jobsearch",
 	build: {
 		assetsDir: "projects/jobsearch/assets"
 	},
